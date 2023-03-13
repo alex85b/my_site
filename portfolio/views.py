ce125c49850e961a5285e8a67c9826683fcfce47
אלכס <58888321+alex85b@users.noreply.github.com>
2023-03-13 02:47:30 +0200

    New design

--- portfolio/views.py
+++ portfolio/views.py
@@ -13,6 +13,12 @@ class IndexView(View):
         display_proj = zip(latest_projects, projects_skills)
         return render(request, "portfolio/index.html", {"projects": display_proj})
 
-    def post(self, request):
-        # This should be reconsidered !
-        return render(request, "portfolio/index.html")
+    # def post(self, request):
+    #     # This should be reconsidered !
+    #     return render(request, "portfolio/index.html")
+
+
+class ProjectDetailsVeiw(View):
+    def get(self, requet, slug):
+        print(f"slug: {slug}")
+        return render(requet, "portfolio/project_details.html")
